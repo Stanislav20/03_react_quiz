be10844c16d604e452405197f5ae074443c6e152
Stanislav20 <stas_on38@mail.ru>
2023-03-15 03:44:18 +0800

    update button

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,7 +5,7 @@ function Result({questions, correctVariant}) {
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="finish" />
       <h2>{`Вы отгадали ${correctVariant} ответа из ${questions.length}`}</h2>
-      <a href="/"><button>Попробовать снова</button></a>
+      <a href="/03_react_quiz"><button>Попробовать снова</button></a>
     </div>
   );
 }
