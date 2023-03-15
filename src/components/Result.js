93faad4099f122df691fd16e229b6dff45308a5e
Stanislav20 <stas_on38@mail.ru>
2023-03-16 01:33:22 +0800

    доработана кнопка "попробовать снова"

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,7 +5,6 @@ function Result({questions, correctVariant, onClickRrepeatTest}) {
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" alt="finish" />
       <h2>{`Вы отгадали ${correctVariant} ответа из ${questions.length}`}</h2>
-      {/* <a href="/03_react_quiz"><button>Попробовать снова</button></a> */}
 			<button onClick={onClickRrepeatTest} >Попробовать снова</button>
     </div>
   );
